cbf6ab1848c8859d9128e036198ff2bf7510f99d
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-08-23 13:31:11 -0500

    type error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-//@ts-check
+import type { NextConfig } from 'next'
  
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
  
-module.exports = nextConfig
+export default nextConfig
